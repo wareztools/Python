d1f1174ad802a9db0d0197039b8d5b19c470b01b
wareztools <126905495+wareztools@users.noreply.github.com>
2023-03-03 23:57:28 +0300

    Add files via upload

---
+++ not_defteri.py
@@ -0,0 +1,53 @@
+print("""
+NOT DEFTERİ
+
+1)Bilgi ekle
+2)Oku
+3)Sil
+4)kapat q
+
+
+""")
+
+while True:
+    işlem = input("işlem seç :")
+
+    if işlem == "1":
+
+        isim_soyisim = input("isim :")
+        sehir = input("sehir :")
+        telefon = int(input("telefon"))
+        print("Bilgiler eklendi ")
+
+
+        root = open("deneme.txt","w",encoding="utf8")
+        root.write("""{}\n {}\n {}\n
+        """.format(isim_soyisim,sehir,telefon))
+        root.close()
+
+
+    elif işlem =="2":
+        root = open("deneme.txt","r",encoding="utf8")
+        print(root.read())
+        root.close()
+
+    elif işlem == "3":
+        root = open("deneme.txt","w",encoding="utf8")
+
+        root.close()
+
+    elif işlem == "q":
+        print("program sonlandırılıyor")
+        break
+
+    else:
+        print("Geçersiz işlem")
+
+
+
+
+
+
+
+
+
